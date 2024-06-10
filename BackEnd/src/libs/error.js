1656ce268e909f617a01c51376293117ba5e55ff
Matheus <matheusmuruci@outlook.com>
2024-06-10 19:25:57 -0300

    feat[error]: Adicionado biblioteca de erros ao backend

---
+++ BackEnd/src/libs/error.js
@@ -0,0 +1,11 @@
+import createError from '@fastify/error'
+
+export const AUTH_NO_TOKEN = createError('AUTH_NO_TOKEN', 'x-access-token is missing', 401);
+export const AUTH_INVALID_TOKEN = createError('AUTH_INVALID_TOKEN', 'The token provided is invalid', 401);
+
+export const AUTH_NO_ADMIN_TOKEN = createError('AUTH_NO_ADMIN_TOKEN', 'admin-token is missing', 401);
+export const AUTH_INVALID_ADMIN_TOKEN = createError('AUTH_INVALID_ADMIN_TOKEN', 'The admin-token provided is invalid', 401);
+
+export const ALREADY_EXISTS = createError('ALREADY_EXISTS', 'The object already exists', 412);
+
+export const PROPERTY_ISADMIN_FALSE = createError('PROPERTY_ISADMIN_FALSE', 'isAdmin property must be true', 400);
