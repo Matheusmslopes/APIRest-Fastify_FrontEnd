cb7aebbe9eab881dc952c5d426ae7888d713a386
Matheus <matheusmuruci@outlook.com>
2024-06-14 14:27:20 -0300

    feat[request]: Adicionado lógica para tratamento de erro

--- FrontEnd/services/request.ts
+++ FrontEnd/services/request.ts
@@ -2,8 +2,20 @@
 export function request<TResponse>(
     url: string,
     config: RequestInit = {}
-):Promise<TResponse> {
+): Promise<TResponse> {
     return fetch(url, config)
-        .then((response) => response.json())
-        .then((response) => response as TResponse);
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.text(); 
+        })
+        .then((responseText) => {
+            console.log('Response Text:', responseText); 
+            try {
+                return JSON.parse(responseText) as TResponse;
+            } catch (error) {
+                throw new Error('Failed to parse JSON: ' + responseText);
+            }
+        });
 }
