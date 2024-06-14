e4cb90ab23a5b4b0abe0d4656687494bfa959953
Matheus <matheusmuruci@outlook.com>
2024-06-13 21:21:57 -0300

    feat[user]: Adicionado propriedade do Admin a rota Login

--- BackEnd/src/routes/user.js
+++ BackEnd/src/routes/user.js
@@ -12,9 +12,10 @@ export default async function user(app, options) {
                 properties: {
                     _id: { type: 'string' },
                     username: { type: 'string' },
-                    password: { type: 'string' }
+                    password: { type: 'string' },
+                    admin: { type: 'boolean' }
                 },
-                required: ['username', 'password']
+                required: ['username', 'password', 'admin']
             }
         }
     }, async (req, rep) => {
@@ -23,46 +24,9 @@ export default async function user(app, options) {
         let result = await users.count({username: user.username})
         if(result <= 0) throw new USER_ALREADY_EXISTS();
 
-        await users.insertOne({ username: username, password: password });
+        await users.insertOne({ username: user.username, password: user.password, admin: user.admin });
 
         return;
-    });
-
-    app.put('/register/:id', {
-        schema: {
-            body: {
-                type: 'object',
-                properties: {
-                    id: { type: 'integer' },
-                    username: { type: 'string' },
-                    password: { type: 'string' },
-                    isAdmin: { type: 'boolean' }
-                },
-                required: ['username', 'password', 'isAdmin']
-            }
-        },
-        config: {
-            requireAuthentication: true
-        }
-    }, async (req, rep) => {
-        let id = req.params.id;
-
-        let adminToken = app.jwt.sign(req.body);
-
-        if(!req.body.isAdmin)
-            return rep.code(400).send({
-                "message": "isAdmin property must be true"
-        })
-
-        await users.updateOne({ _id: new app.mongo.ObjectId(id) }, {
-            $set: {
-                adminToken: adminToken
-            }
-        });
-
-        return rep.code(200).send({
-            "admin-token": adminToken
-        });
     });
 
     app.post('/login', {
@@ -72,21 +36,33 @@ export default async function user(app, options) {
                 properties: {
                     _id: { type: 'string' },
                     username: { type: 'string' },
-                    password: { type: 'string' }
+                    password: { type: 'string' },
+                    admin: { type: 'boolean' },
                 },
                 required: ['username', 'password']
             }
         }
     }, async (req, rep) => {
         let user = req.body;
-        let jwtToken = app.jwt.sign(req.body);
         
-        let result = await users.count({username: user.username})
+        let result = await users.count({username: user.username});
         if(result <= 0) throw new USER_NOT_FOUND();
+        else {
+            let jwtToken = app.jwt.sign(req.body);
 
-        return rep.code(200).send({
-            "x-access-token": jwtToken
-        });
+            let isAdmin = await users.count({username: user.username, admin: true});
+            if(isAdmin <= 0) {
+                return rep.code(200).send({
+                    "x-access-token": jwtToken
+                });
+            } else {
+                let adminToken = app.jwt.sign(req.body);
+
+                return rep.code(200).send({
+                    "x-access-token": jwtToken,
+                    "admin-token": adminToken
+                });
+            }
+        }
     });
 }
-
