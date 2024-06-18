8597512d4d3f8d21abfbd5c032a9c4316a9eabaa
joaolucas <joaolucasdeassis.o@gmail.com>
2024-06-17 21:12:15 -0300

    feat: Adicionado configuração para renderização de imagens

--- FrontEnd/next.config.mjs
+++ FrontEnd/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+          'm.media-amazon.com',
+          'another-domain.com',
+          'yet-another-domain.com',
+          // Adicione mais domínios conforme necessário
+        ],
+      },
+};
 
 export default nextConfig;
